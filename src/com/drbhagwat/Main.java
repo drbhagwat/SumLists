9e6a7a51658ecbb1a19ea03a1e4061425a3deb6f
Dinesh Bhagwat <drbhagwat@yahoo.com>
2022-08-19 22:15:18 +0530

    Fourth Commit

--- src/com/drbhagwat/Main.java
+++ src/com/drbhagwat/Main.java
@@ -6,40 +6,38 @@ import java.util.Scanner;
 
 public class Main {
   /**
-   * Given an integer, this method extracts each digit in that integer (from
-   * last digit to the first digit) and stores each digit into successive
-   * nodes of the linked list.
+   * Given an integer, this method extracts each digit of that integer
+   * and stores them into successive nodes of a linked list.
    *
    * @param i    - an integer - should not be negative
-   * @param list - a list where you need to store each digit of the integer
+   * @param list - a list where you want to store each digit of the integer
    */
   public static void addIntToList(int i, List<Character> list) {
-    if (i < 0) {
-      throw new IllegalArgumentException("The given integer should be >=0");
+    if ((i < 0) || (list == null)) {
+      throw new IllegalArgumentException("The given integer should be >=0 " +
+          "and the list should not be null");
     }
 
-    if (i <= 9) { // the int is 9 max (handles 0-9)
+    if (i <= 9) { // single digit int
       list.add((char) (i + '0'));
-    } else { // the int is at least 10
+    } else { // the int is double digits or more
 
       while (i >= 10) {
         int remainder = i % 10;
         list.add((char) (remainder + '0'));
-        i = i / 10;
+        i /= 10;
       }
       list.add((char) (i + '0'));
     }
-    System.out.println(list);
   }
 
   /**
-   * Given two lists, this method adds each digit stored in
-   * successive nodes of those lists, creates a new list, and
-   * stores the result into that new list
+   * Given two lists, this method adds each digit stored in successive nodes
+   * of those lists and stores the result into that new list
    *
    * @param list1 - first list, represents the first integer backwards
    * @param list2 - second list, represents the second integer backwards
-   * @return - the sum representing the sum, backwards
+   * @return - the new list, representing the sum, backwards
    */
   public static List<Character> add(List<Character> list1,
                                     List<Character> list2) {
@@ -66,7 +64,7 @@ public class Main {
     if (size1 > minimumSize) {
 
       for (int i = 0; i < (size1 - minimumSize); i++) {
-        Character first = list1.get(i);
+        Character first = list1.get(i + minimumSize);
         int sum = (first - '0') + carry;
 
         if (sum > 9) {
@@ -81,7 +79,7 @@ public class Main {
 
       if (size2 > minimumSize) {
         for (int i = 0; i < (size2 - minimumSize); i++) {
-          Character second = list2.get(i);
+          Character second = list2.get(i + minimumSize);
           int sum = (second - '0') + carry;
 
           if (sum > 9) {
